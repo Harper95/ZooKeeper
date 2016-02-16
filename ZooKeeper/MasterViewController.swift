5f02dbdfb09a0dc3a9ed431ddf7c3f329b634f81
Clayton Harper <Clayton_Harper95@yahoo.com>
2016-02-16 01:31:04 -0700

    Added Staff cells to project

--- ZooKeeper/MasterViewController.swift
+++ ZooKeeper/MasterViewController.swift
@@ -9,16 +9,19 @@
 import UIKit
 import SwiftyJSON
 
+let animalKey: Int = 0
+let  staffKey: Int = 1
+
 class MasterViewController: UITableViewController {
-
+    
     var detailViewController: DetailViewController? = nil
-    var animalData: [Animal]?
-
+    var data = [Int:[AnyObject]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.navigationItem.leftBarButtonItem = self.editButtonItem()
-
+        
         let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
         self.navigationItem.rightBarButtonItem = addButton
         if let split = self.splitViewController {
@@ -27,32 +30,41 @@ class MasterViewController: UITableViewController {
         }
         
         tableView.rowHeight = 85
-        animalData = AnimalFactory.zooFromJSONFileNamed("zoo")
+        data[animalKey] = AnimalFactory.zooFromJSONFileNamed("zoo")
+        data[staffKey] = StaffFactory.zooFromJSONFileNamed("zoo")
     }
-
+    
     override func viewWillAppear(animated: Bool) {
         self.clearsSelectionOnViewWillAppear = self.splitViewController!.collapsed
         super.viewWillAppear(animated)
         tableView.reloadData()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     func insertNewObject(sender: AnyObject) {
-//        objects.insert(NSDate(), atIndex: 0)
-//        let indexPath = NSIndexPath(forRow: 0, inSection: 0)
-//        self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+        //        objects.insert(NSDate(), atIndex: 0)
+        //        let indexPath = NSIndexPath(forRow: 0, inSection: 0)
+        //        self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
     }
-
+    
     // MARK: - Segues
-
+    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "AnimalDetail" {
             if let indexPath = self.tableView.indexPathForSelectedRow {
-                let object = animalData![indexPath.row]
+                let object = data[indexPath.row]
+                let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
+                controller.detailItem = object
+                controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
+                controller.navigationItem.leftItemsSupplementBackButton = true
+            }
+        } else if segue.identifier == "StaffDetail" {
+            if let indexPath = self.tableView.indexPathForSelectedRow {
+                let object = data[indexPath.row]
                 let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
                 controller.detailItem = object
                 controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
@@ -60,44 +72,55 @@ class MasterViewController: UITableViewController {
             }
         }
     }
-
+    
     // MARK: - Table View
-
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
+        return data.count
     }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if let data = animalData {
-            return data.count
+        if let section = data[section] {
+            return section.count
         }
         return 0
     }
-
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("AnimalCell", forIndexPath: indexPath) as! AnimalTableViewCell
-
-        let animal: Animal = animalData![indexPath.row]
-        cell.animal = animal
-        cell.configureView()
-        return cell
-        
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCellWithIdentifier("AnimalCell", forIndexPath: indexPath) as! AnimalTableViewCell
+            
+            let animal: Animal = data[animalKey]![indexPath.row] as! Animal
+            cell.animal = animal
+            cell.configureView()
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCellWithIdentifier("StaffCell", forIndexPath: indexPath) as! StaffTableViewCell
+            
+            let staff: Staff = data[staffKey]![indexPath.row] as! Staff
+            cell.staff = staff
+            cell.configureView()
+            return cell
+        }
     }
-
+    
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-
+    
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
-            animalData!.removeAtIndex(indexPath.row)
+            if indexPath.section == 0 {
+                data[animalKey]!.removeAtIndex(indexPath.row)
+            }
+            if indexPath.section == 1{
+                data[staffKey]?.removeAtIndex(indexPath.row)
+            }
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
         }
     }
-
-
 }
 
